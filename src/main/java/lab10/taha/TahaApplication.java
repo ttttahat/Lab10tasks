fea1c427438567927071b3ad35c64ff9b565e36d
Taha K <taha.koza@hotmail.com>
2025-12-15 16:04:57 +0100

    project setup

---
+++ src/main/java/lab10/taha/TahaApplication.java
@@ -0,0 +1,13 @@
+package lab10.taha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TahaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab10/taha/TahaApplicationTests.java
@@ -0,0 +1,13 @@
+package lab10.taha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

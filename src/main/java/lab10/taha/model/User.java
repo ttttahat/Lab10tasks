a807218b95531487dfaa3009417d69bffac496a0
Taha K <taha.koza@hotmail.com>
2025-12-16 09:15:54 +0100

    Api structure design added

--- src/main/java/lab10/taha/model/User.java
+++ src/main/java/lab10/taha/model/User.java
@@ -22,7 +22,7 @@ public class User {
         this.password = password;
     }
 
-    // Getters and setters
+    // getters and setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
